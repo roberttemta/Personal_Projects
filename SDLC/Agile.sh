ca87980ec8ea490053c934c72aa641cedc69ab8b
Robert <roberttemta@gmail.com>
2024-08-02 16:29:26 -0500

    SDLC methodology

---
+++ SDLC/Agile.sh
@@ -0,0 +1,7 @@
+Different SDLC models, such as "Waterfall", "Agile", and "Spiral", offer different approaches to software development. 
+  For example, Agile is a flexible and adaptable process that focuses on people and collaboration. 
+  Some popular Agile frameworks include:
+        -Scrum: Focuses on continuous collaboration, frequent deliveries, and special development cycles called Sprints
+        -Kanban: A scheduling system framework that uses cards to track and support the production system
+        -Extreme Programming (XP): Focuses on project flexibility and writing high quality, well-tested code 
+
---
+++ SDLC/SOC.sh
@@ -0,0 +1,17 @@
+SOC 2 Type 2 is a cybersecurity compliance framework that assesses an organization's 
+ data security controls over a set period of time. 
+It's part of the System and Organization Controls (SOC) suite of services, developed by the American Institute of 
+Certified Public Accountants (AICPA) in 2010. 
+The framework's goal is to ensure that third-party service providers securely store and process customer data, and to 
+    help organizations reduce the risk of security breaches. 
+SOC 2 Type 2 assessments are based on five Trust Services Criteria (TSC) pillars: 
+   -security, 
+   -privacy, 
+   -availability, 
+   -confidentiality, 
+   -and processing integrity. 
+Service organizations can choose which categories are relevant to them and their services. 
+An independent audit firm then evaluates the organization's controls to see how well they've been designed. 
+The results are reported in a SOC 2 Type 2 Report, which can take several months to complete. 
+The report can help organizations demonstrate their security controls, answer security questionnaires, 
+and win more sales deals. 
